47a3bf6bc65472f0a88640e123f0044929865b3d
Chiku <chirantan.mitra@gmail.com>
2011-12-20 00:27:07 +0530

    Handle edge cases

--- hotspot.rb
+++ hotspot.rb
@@ -1,14 +1,31 @@
 class Hotspot
   def initialize(lines)
     @lines = lines
-    @store = {}
+    @store = Hash.new(0)
 
-    @lines.each do |line|
-      @store[line] = @store[line] ? @store[line] + 1 : 1
-    end
+    @lines.map   { |line| line.strip.downcase }
+          .select{ |line| not line.empty? }
+          .each  { |line| @store[line] += 1 }
   end
 
   def on(line)
-    @store[line] ? @store[line] : 0
+    @store[line]
+  end
+
+  def to_s
+    dump = ""
+    sorted_array.each do |key, value|
+      dump << "#{key},#{value}\n"
+    end
+
+    dump
+  end
+
+  private
+
+  def sorted_array
+    @store.sort do |(key1, value1), (key2, value2)|
+      value2 <=> value1
+    end
   end
 end
--- test/hotspot_test.rb
+++ test/hotspot_test.rb
@@ -32,4 +32,64 @@ describe "Hotspot test" do
     hotspot = Hotspot.new(lines)
     hotspot.on("absent.txt").must_equal 0
   end
+
+  it "disregards file case" do
+    lines = [
+      "ABC.TXT"
+    ]
+    hotspot = Hotspot.new(lines)
+    hotspot.on("abc.txt").must_equal 1
+  end
+
+  it "neglects empty lines" do
+    lines = [
+      " "
+    ]
+    hotspot = Hotspot.new(lines)
+    hotspot.on(" ").must_equal 0
+  end
+
+  it "neglects spaces at the extremes of the line" do
+    lines = [
+      "  abc.txt  "
+    ]
+    hotspot = Hotspot.new(lines)
+    hotspot.on("abc.txt").must_equal 1
+  end
+
+  it "neglects linefeeds at the extremes of the line" do
+    lines = [
+      "\n\n  abc.txt  \n\r",
+      "\n\n  abc.txt  \r\n"
+    ]
+    hotspot = Hotspot.new(lines)
+    hotspot.on("abc.txt").must_equal 2
+  end
+
+  it "has a string representation" do
+    lines = [
+      "abc.txt",
+      "efg.txt"
+    ]
+    hotspot = Hotspot.new(lines)
+    hotspot.to_s.must_equal <<-EOS
+abc.txt,1
+efg.txt,1
+    EOS
+  end
+
+  it "string representation has maximum occuring string at the top" do
+    lines = [
+      "abc.txt",
+      "abc.txt",
+      "efg.txt",
+      "efg.txt",
+      "efg.txt"
+    ]
+    hotspot = Hotspot.new(lines)
+    hotspot.to_s.must_equal <<-EOS
+efg.txt,3
+abc.txt,2
+    EOS
+  end
 end
