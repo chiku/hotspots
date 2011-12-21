d1b644c3fc4bb246c36234959fb20685d9ae0f33
Chiku <chirantan.mitra@gmail.com>
2011-12-21 07:49:45 +0530

    Pull out option parsing into a separate object

--- hotspot.rb
+++ hotspot.rb
@@ -1,3 +1,5 @@
+require 'optparse'
+
 class Hotspot
   def initialize(lines, cutoff=0)
     @lines  = lines
@@ -31,49 +33,80 @@ class Hotspot
   end
 end
 
-if __FILE__ == $0
-  require 'optparse'
+class HotspotOptionsParser
+  def initialize
+    @options = {
+      :time => 15,
+      :repository => ".",
+      :filter => "",
+      :cutoff => 0
+    }
+  end
 
-  options = {
-    :time => 15,
-    :repository => ".",
-    :filter => "",
-    :cutoff => 0
-  }
+  def parse
+    @parsed ||= OptionParser.new do |opts|
+      set_banner_on(opts)
 
-  OptionParser.new do |opts|
+      handle_time_on(opts)
+      handle_path_on(opts)
+      handle_filter_on(opts)
+      handle_cutoff_on(opts)
+
+      handle_help_on(opts)
+    end.parse!
+
+    @options
+  end
+
+  private
+
+  def set_banner_on(opts)
     opts.banner = "Tool to find most modified files over the past in a git repository."
 
     opts.separator ""
     opts.separator "Usage: ruby #{__FILE__} [options]"
     opts.separator ""
     opts.separator "Specific options:"
+  end
 
+  def handle_time_on(opts)
     opts.on("-t", "--time [TIME]", OptionParser::DecimalInteger,
             "Time is days to scan the repository for. Defaults to fifteen") do |o|
-      options[:time] = o
+      @options[:time] = o
     end
+  end
 
+  def handle_path_on(opts)
     opts.on("-r", "--repository [PATH]", String,
             "The path to the current repository. Defaults to current path") do |o|
-      options[:repository] = o
+      @options[:repository] = o
     end
+  end
 
+  def handle_filter_on(opts)
     opts.on("-f", "--filter [REGEX]", String,
             "The regular expression for file to filter with. All files are allowed when not specified") do |o|
-      options[:filter] = o
+      @options[:filter] = o
     end
+  end
 
+  def handle_cutoff_on(opts)
     opts.on("-c", "--cutoff [CUTOFF]", OptionParser::DecimalInteger,
             "The minimum occurance to consider for a file to appear in the list. Defaults to zero") do |o|
-      options[:cutoff] = o.to_i
+      @options[:cutoff] = o.to_i
     end
+  end
 
+  def handle_help_on(opts)
     opts.on_tail("-h", "--help", "Show this message") do
       puts opts
       exit
     end
-  end.parse!
+  end
+end
+
+if __FILE__ == $0
+  options = HotspotOptionsParser.new.parse
 
   files = %x(
     cd #{options[:repository]} && \
@@ -81,7 +114,7 @@ if __FILE__ == $0
     while read commit_hash
     do
       git show --oneline --name-only $commit_hash | tail -n+2 | grep "#{options[:filter]}"
-    done && \
+    done &&
     cd -
   ).to_s.split("\n")
 
