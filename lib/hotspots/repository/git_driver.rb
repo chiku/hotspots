5357b5d3201659ffb0602a7597712bc5f48d55f3
Chiku <chirantan.mitra@gmail.com>
2011-12-22 07:08:48 +0530

    Move git logic into GitDriver

--- lib/hotspots/repository/git_driver.rb
+++ lib/hotspots/repository/git_driver.rb
@@ -1,6 +1,18 @@
 module Hotspots
   module Repository
     class GitDriver
+      def self.files(options)
+        files = %x(
+          bash -c '\
+            cd #{options[:repository]} && \
+            git log --pretty="%H" --since #{options[:time]}.days.ago |
+            while read commit_hash
+              do
+              git show --oneline --name-only $commit_hash | tail -n+2
+            done
+          '
+        ).split("\n")
+      end
     end
   end
 end
