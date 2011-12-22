a21dad4cc77fa904826fa027cb0969f200760c0c
Chiku <chirantan.mitra@gmail.com>
2011-12-22 23:25:43 +0530

    Remove dependency on bash

--- lib/hotspots/repository/git_driver.rb
+++ lib/hotspots/repository/git_driver.rb
@@ -1,17 +1,26 @@
 module Hotspots
   module Repository
     class GitDriver
-      def self.files(options)
-        files = %x(
-          bash -c '\
-            cd #{options[:repository]} && \
-            git log --pretty="%H" --since #{options[:time]}.days.ago |
-            while read commit_hash
-              do
-              git show --oneline --name-only $commit_hash | tail -n+2
-            done
-          '
-        ).split("\n")
+      def initialize(options)
+        @repository = options[:repository]
+        @time       = options[:time]
+      end
+
+      def files
+        validate!
+        Dir.chdir(@repository)
+
+        commit_hashes = %x(git log --pretty="%H" --since #{@time}.days.ago).gsub("\r", "").split("\n")
+        commit_hashes.map do |commit_hash|
+          %x(git show --oneline --name-only #{commit_hash}).gsub("\r", "").split("\n")[1..-1]
+        end.flatten
+      end
+
+      def validate!
+        if not File.directory?(@repository) and not File.directory?(File.join(@repository, '.git'))
+          puts "#{@repository} doesn't seem to be a git repository!"
+          exit 2
+        end
       end
     end
   end
