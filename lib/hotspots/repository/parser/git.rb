293aeef4c44142065c19f51a2165b3719cba1ee6
Chiku <chirantan.mitra@gmail.com>
2013-04-27 21:07:01 +0530

    Defer how line-endings behave to ruby.
    This will allow OS to take care of various line-endings on different platforms.

--- lib/hotspots/repository/parser/git.rb
+++ lib/hotspots/repository/parser/git.rb
@@ -10,24 +10,16 @@ class Hotspots
           @message_filters = options[:message_filters]
         end
 
-        # TODO : replace with each_line
         def files
-          filtered_commit_hashes.map do |commit_hash|
-            driver.show_one_line_names(:commit_hash => commit_hash).
-              gsub("\r\n", "\n").
-              gsub("\r", "\n").
-              split("\n")[1..-1]
-          end.flatten
+          filtered_commit_hashes.reduce([]) do |acc, commit_hash|
+            acc + driver.show_one_line_names(:commit_hash => commit_hash).lines.map(&:strip)[1..-1]
+          end
         end
 
-        # TODO : replace with each_line
         def filtered_commit_hashes
-          message_filters.map do |filter|
-            driver.pretty_log(:since_days => time, :message_filter => filter).
-              gsub("\r\n", "\n").
-              gsub("\r", "\n").
-              split("\n")
-          end.flatten.uniq
+          message_filters.reduce([]) do |acc, filter|
+            acc + driver.pretty_log(:since_days => time, :message_filter => filter).lines.map(&:strip)
+          end.uniq
         end
       end
     end
--- test/hotspots/repository/parser/git_test.rb
+++ test/hotspots/repository/parser/git_test.rb
@@ -41,20 +41,11 @@ module Hotspots::Repository
       git_parser.files.must_equal(["file1", "file2", "file2", "file3", "file5", "file4"])
     end
 
-    describe "handles line ending" do
-      it "of type \r\n" do
-        options    = {:time => 10, :message_filters => ["Foo", "Bar"]}
-        git_parser = Parser::Git.new StubGitDriver.new(:line_ending => "\r\n"), options
+    it "handles line ending" do
+      options    = {:time => 10, :message_filters => ["Foo", "Bar"]}
+      git_parser = Parser::Git.new StubGitDriver.new, options
 
-        git_parser.files.must_equal(["file1", "file2", "file2", "file3", "file5", "file4"])
-      end
-
-      it "of type \r" do
-        options    = {:time => 10, :message_filters => ["Foo", "Bar"]}
-        git_parser = Parser::Git.new StubGitDriver.new(:line_ending => "\r"), options
-
-        git_parser.files.must_equal(["file1", "file2", "file2", "file3", "file5", "file4"])
-      end
+      git_parser.files.must_equal(["file1", "file2", "file2", "file3", "file5", "file4"])
     end
   end
 
@@ -68,14 +59,12 @@ module Hotspots::Repository
     it "has a sane show one line names" do
       StubGitDriver.new.show_one_line_names(:commit_hash => "SHA2").
           must_equal "SHA1 commit message\nfile2\nfile3\nfile5"
-      StubGitDriver.new(:line_ending => "\r\n").show_one_line_names(:commit_hash => "SHA2").
-          must_equal "SHA1 commit message\r\nfile2\r\nfile3\r\nfile5"
     end
   end
 
   class StubGitDriver
-    def initialize(options = {})
-      line_ending = options[:line_ending] || "\n"
+    def initialize
+      line_ending = "\n"
       @pretty_log_enum = ["SHA1#{line_ending}SHA2", "SHA2#{line_ending}SHA3"].cycle
       @commits = {
         "SHA1" => "SHA1 commit message#{line_ending}file1#{line_ending}file2",
