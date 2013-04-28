4fc252f0f446c671b55b03b481648272a9bd8001
Chiku <chirantan.mitra@gmail.com>
2013-04-28 23:22:50 +0530

    Using cycle caused ruby 1.9.2-p320 to segfault on Arch linux. Use an explicit cycle in tests

--- test/hotspots/repository/parser/git_test.rb
+++ test/hotspots/repository/parser/git_test.rb
@@ -4,16 +4,19 @@ module Hotspots::Repository
   class StubGitDriver
     def initialize
       line_ending = "\n"
-      @pretty_log_enum = ["SHA1#{line_ending}SHA2", "SHA2#{line_ending}SHA3"].cycle
+      @pretty_log = ["SHA1#{line_ending}SHA2", "SHA2#{line_ending}SHA3"]
       @commits = {
         "SHA1" => "SHA1 commit message#{line_ending}file1#{line_ending}file2",
         "SHA2" => "SHA1 commit message#{line_ending}file2#{line_ending}file3#{line_ending}file5",
         "SHA3" => "SHA1 commit message#{line_ending}file4",
       }
+      @pretty_log_cycle = 0
     end
 
     def pretty_log(options)
-      @pretty_log_enum.next
+      @pretty_log[@pretty_log_cycle].tap do
+        @pretty_log_cycle = (@pretty_log_cycle + 1) % 2
+      end
     end
 
     def show_one_line_names(options)
@@ -22,17 +25,18 @@ module Hotspots::Repository
   end
 
   describe StubGitDriver do
+    subject { StubGitDriver.new }
+
     describe "#pretty_log" do
       it "is sane" do
-        driver = StubGitDriver.new
-        driver.pretty_log(:time => 10, :message_filter => "Foo").must_equal "SHA1\nSHA2"
-        driver.pretty_log(:time => 10, :message_filter => "Bar").must_equal "SHA2\nSHA3"
+        subject.pretty_log(:time => 10, :message_filter => "Foo").must_equal "SHA1\nSHA2"
+        subject.pretty_log(:time => 10, :message_filter => "Bar").must_equal "SHA2\nSHA3"
       end
     end
 
     describe "#show_one_line_names" do
       it "is sane" do
-        StubGitDriver.new.show_one_line_names(:commit_hash => "SHA2").
+        subject.show_one_line_names(:commit_hash => "SHA2").
             must_equal "SHA1 commit message\nfile2\nfile3\nfile5"
       end
     end
