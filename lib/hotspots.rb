4c3769065b855fefd2f7720bbc3e117d2c6b3f30
Chirantan Mitra <chirantan.mitra@gmail.com>
2015-04-11 11:01:26 +0530

    Reduce the use of instance level variables

--- lib/hotspots.rb
+++ lib/hotspots.rb
@@ -6,20 +6,14 @@ require "hotspots/options_parser"
 require "hotspots/repository"
 
 class Hotspots
+  attr_reader :configuration
+
   def initialize(configuration)
-    @configuration   = configuration
-    @logger          = @configuration.logger
-    @repository      = @configuration.repository
-    @exit_strategy   = @configuration.exit_strategy
-    @time            = @configuration.time
-    @message_filters = @configuration.message_filters
-    @file_filter     = @configuration.file_filter
-    @cutoff          = @configuration.cutoff
+    @configuration = configuration
   end
 
   def output
     validate
-    assign
     inside_repository do
       run
     end
@@ -28,29 +22,38 @@ class Hotspots
   private
 
   def validate
-    @exit_strategy.perform
-    prepare_for_exit_if_git_status_invalid
-    @exit_strategy.perform
-  end
-
-  def assign
-    @driver = Hotspots::Repository::GitDriver.new(:logger => @logger)
-    @parser = Hotspots::Repository::GitParser.new(@driver, :time => @time, :message_filters => @message_filters)
-    @store  = Hotspots::Store.new(@parser.files, :cutoff => @cutoff, :file_filter => @file_filter)
+    configuration.exit_strategy.perform
+    ensure_git_repository
   end
 
   def inside_repository
-    yield Dir.chdir(@repository)
+    yield Dir.chdir(repository)
   end
 
   def run
-    puts @store.to_s
+    puts store.to_s
   end
 
-  def prepare_for_exit_if_git_status_invalid
+  def ensure_git_repository
     `git status 2>&1`
     unless $? == 0
-      @exit_strategy = Hotspots::Exit::Error.new(:message => "'#{@repository}' doesn't seem to be a git repository!", :code => 10)
+      Hotspots::Exit::Error.new(:message => "'#{@repository}' doesn't seem to be a git repository!", :code => 10).perform
     end
+  end
+
+  def repository
+    configuration.repository
+  end
+
+  def store
+    Hotspots::Store.new(parser.files, :cutoff => configuration.cutoff, :file_filter => configuration.file_filter)
+  end
+
+  def parser
+    Hotspots::Repository::GitParser.new(driver, :time => configuration.time, :message_filters => configuration.message_filters)
+  end
+
+  def driver
+    Hotspots::Repository::GitDriver.new(:logger => configuration.logger)
   end
 end
